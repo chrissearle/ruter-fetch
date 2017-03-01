4afbac6bd7beb5051aa4c8ddfae42bd98dbc6201
Chris Searle <chris@chrissearle.org>
2017-03-01 21:01:20 +0100

    Show two rotating pages

--- arduino/ruter.ino
+++ arduino/ruter.ino
@@ -14,8 +14,19 @@ U8G2_PCD8544_84X48_1_4W_SW_SPI u8g2(U8G2_R0, CLK_PIN, DIN_PIN, CE_PIN, DC_PIN, R
 
 u8g2_uint_t rowHeight = 12;
 
-void setup(void)
-{
+char *lines[] = {
+  "Mot Sentrum",
+  "4 Bergkr-19:34",
+  "5 Vestli-19:39",
+  "5 Ringen-19:43",
+  "Fra sentrum",
+  "4 Vestli-19:39",
+  "5 Sognsv-19:42",
+  "5 Ringen-19:50"
+};
+
+
+void setup(void) {
   pinMode(LIGHT_PIN, OUTPUT);
   digitalWrite(LIGHT_PIN, LOW);	
   
@@ -23,39 +34,27 @@ void setup(void)
 }
 
 void drawLineAtHeight(u8g2_uint_t height) {
-    u8g2.drawLine(0, height, 84, height);
+  u8g2.drawLine(0, height, 84, height);
 }
 
 void drawTitle(const char *title) {
-    u8g2.setFont(u8g2_font_6x13_tr);
-    u8g2.drawStr(3,10,title);
+  u8g2.setFont(u8g2_font_6x13_tr);
+  u8g2.drawStr(3,10,title);
 }
 
 void drawTextInRow(int row, const char *text) {
-    u8g2.setFont(u8g2_font_5x8_tr);
-    u8g2.drawStr(7,((row + 1) * rowHeight) - 2,text);
+  u8g2.setFont(u8g2_font_5x8_tr);
+  u8g2.drawStr(7,((row + 1) * rowHeight) - 2,text);
 }
 
 void drawPage(char *lines[], int start, int count) {
-    drawTitle(lines[start]);
-    for(int i = 1; i < count; i++) {
-      drawTextInRow(i,lines[start + i]);
-    }
+  drawTitle(lines[start]);
+  for(int i = 1; i < count; i++) {
+    drawTextInRow(i,lines[start + i]);
+  }
 }
 
-void loop(void)
-{
-  char *lines[] = {
-    "Mot Sentrum",
-    "4 Bergkr-19:34",
-    "5 Vestli-19:39",
-    "5 Ringen-19:43",
-    "Fra sentrum",
-    "4 Vestli-19:39",
-    "5 Sognsv-19:42",
-    "5 Ringen-19:50"
-  };
-
+void showPage(int offset) {
   u8g2.firstPage();
   
   do {
@@ -63,7 +62,15 @@ void loop(void)
     drawLineAtHeight(rowHeight * 2);
     drawLineAtHeight(rowHeight * 3);
 
-    drawPage(lines, 4, 4);
+    drawPage(lines, offset, 4);
 
   } while ( u8g2.nextPage() );
+
+}
+
+void loop(void) {
+  showPage(0);
+  delay(5000);
+  showPage(4);
+  delay(5000);
 }
