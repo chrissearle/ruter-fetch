2c6845f4348b07fd35232798f8089819914cdae6
Chris Searle <chris@chrissearle.org>
2017-03-02 08:49:35 +0100

    Update to output to file

--- script/src/index.js
+++ script/src/index.js
@@ -1,18 +1,47 @@
 import ruter from 'ruter-api'
 import moment from 'moment'
+import { forEach, chunk, range } from 'lodash'
+import { createWriteStream } from 'fs'
+
+function outputLines(lines) {
+    if (process.argv.length > 2) {
+        const file = createWriteStream(process.argv[2])
+
+        file.on('error', (err) => {
+            console.log(err)
+        })
+
+        file.write(lines)
+
+        file.end()
+    } else {
+        console.log(lines)
+    }
+}
+
+function chunksToString(title, chunks) {
+    const result = []
+
+    if (chunks.length > 0) {
+        result.push(title)
+        result.push(...chunks[0])
+    }
+
+    if (chunks.length > 1) {
+        result.push(`${title} 2`)
+        result.push(...chunks[1])
+    }
+
+    return result.join('\n')
+}
 
 ruter.api('StopVisit/GetDepartures/3010360', {}, response => {
-    let maxCount = 6
-
     const json = response['result']
-    if (json.length < maxCount) {
-        maxCount = json.length
-    }
 
     const mot = []
     const fra = []
 
-    json.slice(0, maxCount).map((item) => {
+    json.map((item) => {
         const mvj = item['MonitoredVehicleJourney']
         const mc = mvj['MonitoredCall']
 
@@ -25,18 +54,18 @@ ruter.api('StopVisit/GetDepartures/3010360', {}, response => {
 
         const display = `${line} ${route.substring(0, 6)}-${ts}`
 
-        if (platform === '1'
-        ) {
+        if (platform === '1') {
             mot.push(display)
-        }
-        else {
+        } else {
             fra.push(display)
         }
     })
 
-    console.log('Mot sentrum')
-    console.log(mot.join('\n'))
+    let lines = [chunksToString("Mot sentrum", chunk(mot, 3)), chunksToString("Fra sentrum", chunk(fra, 3))].join('\n')
 
-    console.log('Fra sentrum')
-    console.log(fra.join('\n'))
+    forEach(range(16 - lines.split('\n').length), () => {
+        lines = `${lines}\n`
+    })
+
+    outputLines(lines)
 })
